c2a61aa6e33c6b27e6565c8125c755e12d00fd5c
ronnyhdez <ronny.hernandezm@gmail.com>
2023-02-09 14:00:24 -0700

    Ref #1 include jsx expression to modify value dinamically

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,16 +2,22 @@ import React, { Component } from 'react';
 
 class Counter extends Component { 
     state = {
-        count: 1
+        count: 0
     };
 
     render() { 
         return (
             <React.Fragment>
-                <span>{this.state.count}</span>
+                <span>{this.formatCount()}</span>
                     <button>Increment</button>
             </React.Fragment>
         );    
+    }
+
+    formatCount() {
+
+        const { count } = this.state;
+        return count === 0 ? 'Zero' : count;
     }
 }
  
